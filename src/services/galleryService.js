c1a9270628afee4ec917df1487d0705db51e560b
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:57 +0200

    feat(services): add galleryService with album support

---
+++ src/services/galleryService.js
@@ -0,0 +1,14 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const galleryService = {
+  getAll:       (params = {}) => api.get(/gallery\),
+  getById:      (id)          => api.get(/gallery/\),
+  upload:       (formData)    => api.post('/gallery/upload', formData, { headers: { 'Content-Type': 'multipart/form-data' } }),
+  update:       (id, data)    => api.put(/gallery/\, data),
+  remove:       (id)          => api.delete(/gallery/\),
+  getAlbums:    ()            => api.get('/gallery/albums'),
+  createAlbum:  (data)        => api.post('/gallery/albums', data),
+  deleteAlbum:  (id)          => api.delete(/gallery/albums/\),
+};
+export default galleryService;
