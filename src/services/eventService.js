e62eec1f68a19241bc659f032b17bee1c236293d
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:56 +0200

    feat(services): add eventService CRUD operations

---
+++ src/services/eventService.js
@@ -0,0 +1,12 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const eventService = {
+  getAll:   (params = {}) => api.get(/events\),
+  getById:  (id)          => api.get(/events/\),
+  create:   (data)        => api.post('/events', data),
+  update:   (id, data)    => api.put(/events/\, data),
+  remove:   (id)          => api.delete(/events/\),
+  cancel:   (id)          => api.patch(/events/\/cancel),
+};
+export default eventService;
