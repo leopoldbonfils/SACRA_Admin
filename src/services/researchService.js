658f667ea7cb9accb6c28b010cd11347222c440f
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:57 +0200

    feat(services): add researchService CRUD operations

---
+++ src/services/researchService.js
@@ -0,0 +1,12 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const researchService = {
+  getAll:   (params = {}) => api.get(/research\),
+  getById:  (id)          => api.get(/research/\),
+  create:   (data)        => api.post('/research', data),
+  update:   (id, data)    => api.put(/research/\, data),
+  remove:   (id)          => api.delete(/research/\),
+  publish:  (id)          => api.patch(/research/\/publish),
+};
+export default researchService;
