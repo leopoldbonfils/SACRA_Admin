8df559534c608e1b8428c56dc160a0f01c2df4e2
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:56 +0200

    feat(services): add newsService CRUD operations

---
+++ src/services/newsService.js
@@ -0,0 +1,13 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const newsService = {
+  getAll:   (params = {}) => api.get(/news\),
+  getById:  (id)          => api.get(/news/\),
+  create:   (data)        => api.post('/news', data),
+  update:   (id, data)    => api.put(/news/\, data),
+  remove:   (id)          => api.delete(/news/\),
+  publish:  (id)          => api.patch(/news/\/publish),
+  archive:  (id)          => api.patch(/news/\/archive),
+};
+export default newsService;
