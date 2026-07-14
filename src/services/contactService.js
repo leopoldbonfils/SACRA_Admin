c9d11a166c77e31bce78b871cc607ba8291be969
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:58 +0200

    feat(services): add contactService for messages

---
+++ src/services/contactService.js
@@ -0,0 +1,12 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const contactService = {
+  getAll:    (params = {}) => api.get(/contact\),
+  getById:   (id)          => api.get(/contact/\),
+  markRead:  (id)          => api.patch(/contact/\/read),
+  reply:     (id, message) => api.post(/contact/\/reply, { message }),
+  remove:    (id)          => api.delete(/contact/\),
+  getUnread: ()            => api.get('/contact/unread/count'),
+};
+export default contactService;
