0baf028553df9a307ab23df19ab3a67635cf493f
leopoldbonfils <leopoldbonfils@github.com>
2026-07-14 15:56:56 +0200

    feat(services): add videoService CRUD operations

---
+++ src/services/videoService.js
@@ -0,0 +1,11 @@
+﻿import api from './api';
+import { buildQueryString } from '../utils/helpers';
+
+const videoService = {
+  getAll:   (params = {}) => api.get(/videos\),
+  getById:  (id)          => api.get(/videos/\),
+  create:   (data)        => api.post('/videos', data),
+  update:   (id, data)    => api.put(/videos/\, data),
+  remove:   (id)          => api.delete(/videos/\),
+};
+export default videoService;
