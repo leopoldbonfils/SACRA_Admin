1c035968e6703363655165edbf16adedb8b7e988
leopoldbonfils <leopoldbonfils@github.com>
2026-07-16 15:58:09 +0200

    Update/Implement GeneralSettings.js based on layouts mockup

--- src/pages/settings/GeneralSettings.js
+++ src/pages/settings/GeneralSettings.js
@@ -1,13 +1,8 @@
-﻿import React from 'react';
-import PageHeader from '../../components/layout/PageHeader';
+import React from 'react';
+import WebsiteSettings from './WebsiteSettings';
 
-const GeneralSettings = () => (
-  <div className="page-enter">
-    <PageHeader title="General Settings" subtitle="Coming soon." />
-    <div className="card" style={{ marginTop: 24, padding: 32 }}>
-      <p style={{ color: 'var(--text-muted)' }}>This page is under construction.</p>
-    </div>
-  </div>
-);
+const GeneralSettings = () => {
+  return <WebsiteSettings />;
+};
 
 export default GeneralSettings;
