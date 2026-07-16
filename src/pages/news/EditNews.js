85a94224be47003fc564a166e7c49d02fc917557
leopoldbonfils <leopoldbonfils@github.com>
2026-07-16 15:25:20 +0200

    Update/Implement EditNews.js based on layout mockup

--- src/pages/news/EditNews.js
+++ src/pages/news/EditNews.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import PageHeader from '../../components/layout/PageHeader';
-import { ROUTES } from '../../routes/routeConfig';
+import CreateNews from './CreateNews';
 
-const EditNews = () => (
-  <div className="page-enter">
-    <PageHeader
-      title="Edit Article"
-      breadcrumbs={[{ label: 'Dashboard', to: ROUTES.DASHBOARD }, { label: 'News', to: ROUTES.NEWS }, { label: 'Edit' }]}
-    />
-    <div className="card" style={{ marginTop: 24, padding: 32 }}>
-      <p style={{ color: 'var(--text-muted)' }}>Article editor coming soon.</p>
-    </div>
-  </div>
-);
+const EditNews = () => {
+  return <CreateNews />;
+};
 
 export default EditNews;
